8c3d01687028e37ee708a8c4e27392bea1c451dc
Apolywrath <Apolywrath@gmail.com>
2022-12-08 15:18:27 +0100

    Kommenterat min kod och uppdaterat den lite

--- CarsOOPGruppProjekt/sqlConnection.cs
+++ CarsOOPGruppProjekt/sqlConnection.cs
@@ -18,39 +18,42 @@ namespace CarsOOPGruppProjekt
         }
         private MySql.Data.MySqlClient.MySqlConnection mysqlCon()
         {
+
+            //Declarerar connectionString till null
             string connetionString = null;
+            //Delcarerar en ny MySqlConnection som används för att ansluta till Mysql
             MySqlConnection cnn;
+            //Själva connectionString Vilken server, Localhost = din egna dator. Database vad cars heter. uid = User_id. pwn = lösenord
             connetionString = "server=localhost;database=cars;uid=oopgrund;pwd=programmering;";
+            //Försöker att connecta misslyckas det få ett felmeddelande
             cnn = new MySqlConnection(connetionString);
             try
             {
                 cnn.Open();
-                //MessageBox.Show("Connection Open ! ");
                 cnn.Close();
             }
             catch (Exception ex)
             {
                 //MessageBox.Show("Can not open connection ! ");
             }
+            //Retunerar denna MySqlConnection så det finns i en local variable.
             return cnn;
         }
 
         public List<Cars> getData()
         {
+            // Använder den lokala MysqlConnection för att öppna en connection
             _mysqlCon.Open();
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = "SELECT * FROM CARS";
-            cmd.Connection = _mysqlCon;
-            MySqlDataReader reader = cmd.ExecuteReader();
-            List<Cars> test = new List<Cars>();
+            //Skapar en MysqlDataReader  genom att skapa en ny mysqlcommand med query + connection
+            MySqlDataReader reader = new MySqlCommand("SELECT * FROM CARS", _mysqlCon).ExecuteReader();
+            //Skapar en temp List med classen Cars
+            List<Cars> sqlCarList = new List<Cars>();
+
+            // Läser varje rad tills det inte finns fler rader.
             while(reader.Read())
             {
-                /* Cars volvo = new Cars(Int32.Parse(reader["cars_id"].ToString()), reader["cars_model"].ToString(), reader["manufacturers_manufacturers_name"].ToString(),
-                     reader["cars_year"].ToString() , reader["retailers_retailers_name"].ToString(), Int32.Parse(reader["cars_price"].ToString())
-                     );
-                 test.Add(volvo); */
-
-                test.Add(new Cars(
+                // Lägger till i våran Lista sqlCarList med hjälp av namnen va det heter i databasen
+                sqlCarList.Add(new Cars(
                     Convert.ToInt32(reader["cars_id"].ToString()),
                     reader["cars_model"].ToString(),
                     reader["manufacturers_manufacturers_name"].ToString(),
@@ -59,14 +62,19 @@ namespace CarsOOPGruppProjekt
                     Convert.ToInt32(reader["cars_price"])));
 
             }
+            //Stänger mysqlconnection
             _mysqlCon.Close();
-            return test;
+            //Retunerar en lista med alla bilar ifrån databasen
+            return sqlCarList;
         }
 
+        // Har en input query som försöker köras där man bara får tillbaka en int rowAffected hur många rader som påverkas
         public void updateData(string query)
         {
+            _mysqlCon.Open();
             MySqlCommand cmd = new MySqlCommand(query,_mysqlCon);
             int rowAffected = cmd.ExecuteNonQuery();
+            _mysqlCon.Close();
         }
 
 
