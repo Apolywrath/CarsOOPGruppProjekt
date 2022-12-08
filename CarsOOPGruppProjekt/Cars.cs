806ff09823702c38ce7be0fecd6dc2bef1cc649e
Apolywrath <Apolywrath@gmail.com>
2022-12-08 11:28:17 +0100

    Added classes + MYSQLDATA

---
+++ CarsOOPGruppProjekt/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Cars
+{
+
+    public int cars_id;
+    public string cars_model;
+    public string manufacturers_manufacturers_name;
+    public string cars_year;
+    public string retailers_retailers_name;
+    public int cars_price;
+    public Cars(int cars_id, string cars_model, string manufacturers_manufacturers_name, string cars_year
+        , string retailers_retailers_name, int cars_price)
+	{
+        this.cars_id = cars_id;
+        this.cars_model = cars_model;
+        this.manufacturers_manufacturers_name = manufacturers_manufacturers_name;
+        this.cars_year = cars_year;
+        this.retailers_retailers_name = retailers_retailers_name;
+        this.cars_price = cars_price;
+    }
+}
---
+++ CarsOOPGruppProjekt/sqlConnection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WinFormsApp1
+{
+    internal class sqlConnection
+    {
+
+        public MySql.Data.MySqlClient.MySqlConnection _mysqlCon;
+
+        public sqlConnection()
+        {
+            _mysqlCon = mysqlCon();
+        }
+        private MySql.Data.MySqlClient.MySqlConnection mysqlCon()
+        {
+            string connetionString = null;
+            MySqlConnection cnn;
+            connetionString = "server=localhost;database=cars;uid=oopgrund;pwd=programmering;";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                //MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Can not open connection ! ");
+            }
+            return cnn;
+        }
+
+        public List<Cars> getData()
+        {
+            _mysqlCon.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "SELECT * FROM CARS";
+            cmd.Connection = _mysqlCon;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<Cars> test = new List<Cars>();
+            while(reader.Read())
+            {
+                Cars volvo = new Cars(Int32.Parse(reader["cars_id"].ToString()), reader["cars_model"].ToString(), reader["manufacturers_manufacturers_name"].ToString(),
+                    reader["cars_year"].ToString() , reader["retailers_retailers_name"].ToString(), Int32.Parse(reader["cars_price"].ToString())
+                    );
+                test.Add(volvo);
+            }
+            _mysqlCon.Close();
+            return test;
+        }
+
+        public void updateData(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query,_mysqlCon);
+            int rowAffected = cmd.ExecuteNonQuery();
+        }
+
+
+
+    }
+}
+
