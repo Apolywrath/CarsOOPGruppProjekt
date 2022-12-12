854acd49b0ba6695274d97db5d8c4b1f2f5fabb8
henrik <Apolywrath@gmail.com>
2022-12-12 10:44:07 +0100

    Fixed null refrense

--- CarsOOPGruppProjekt/Form1.cs
+++ CarsOOPGruppProjekt/Form1.cs
@@ -82,14 +82,29 @@ namespace CarsOOPGruppProjekt
         // Get current selected Row Data from dataGridView1 for easier handling.
         private Cars getSelectedDataGridViewCar()
         {
-            Cars car = new Cars(
-                Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value.ToString()),
-                dataGridView1.SelectedRows[0].Cells[2].Value.ToString(),
-                dataGridView1.SelectedRows[0].Cells[1].Value.ToString(),
-                dataGridView1.SelectedRows[0].Cells[4].Value.ToString(),
-                dataGridView1.SelectedRows[0].Cells[3].Value.ToString(), 
-                Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[5].Value.ToString()));
+            DataGridViewRow dataRow;
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                dataRow = dataGridView1.SelectedCells[0].OwningRow;
+                Cars car = new Cars(
+                Convert.ToInt32(dataRow.Cells[0].Value.ToString()),
+                dataRow.Cells[2].Value.ToString(),
+                dataRow.Cells[1].Value.ToString(),
+                dataRow.Cells[4].Value.ToString(),
+                dataRow.Cells[3].Value.ToString(),
+                Convert.ToInt32(dataRow.Cells[5].Value.ToString()));
                 return car;
+            }
+            else
+            {
+                Cars car = new Cars(
+                    0, "Default", "Default", "Default", "Default", 0);
+                return car;
+            }
+
+            
+
+            
         }
         private void btnRemove_Click(object sender, EventArgs e)
         {
